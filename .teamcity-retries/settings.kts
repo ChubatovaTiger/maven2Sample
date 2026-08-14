8c4401e26ed2f750aa803a1d90a01002f8a48da8
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-08-14 11:08:41 +0200

    Update settings.kts

--- .teamcity-retries/settings.kts
+++ .teamcity-retries/settings.kts
@@ -84,9 +84,9 @@ object Build2 : BuildType({
             id = "simpleRunner"
             scriptContent = """
                 echo a > a.txt
-                if (( RANDOM % 2 )); then
+                //if (( RANDOM % 2 )); then
                   exit 1
-                fi
+                //fi
             """.trimIndent()
         }
     }
